9c48968572b60135476a608538b4a422276121a0
faustojc <faustojcboko@gmail.com>
2023-08-31 16:26:09 +0800

    Used CORS and changed port of backend server

--- api/server.py
+++ api/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/home', methods=['GET'])
 def return_home():
     return jsonify({"message": "Hello World!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
