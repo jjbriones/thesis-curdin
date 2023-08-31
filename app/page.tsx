614e48ba7de6923f3b27958da5ad5cf9203576c0
faustojc <faustojcboko@gmail.com>
2023-08-31 16:26:47 +0800

    Implemented fetch API from backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div className="text-red-500">Curdin</div>;
+    useEffect(() => {
+        fetch("http://127.0.0.1:8080/api/home")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }, []);
+
+    return <div className="text-red-500">Curdin</div>;
 }
