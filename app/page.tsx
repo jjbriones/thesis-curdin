0017c187297f21a258747038649006b83c98c626
faustojc <faustojcboko@gmail.com>
2023-08-31 16:56:30 +0800

    Testing storing value from server to client

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 'use client';
-import { useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Home() {
+    const [values, setValues] = useState({
+        message: ""
+    });
+
     useEffect(() => {
         fetch("http://127.0.0.1:8080/api/home")
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            console.log(data)
+            setValues({
+                message: data.message
+            });
+        });
     }, []);
 
-    return <div className="text-red-500">Curdin</div>;
+    return (
+        <>
+            <div className={"font-bold"}>
+                Message from server: 
+                <Suspense fallback={"Loading..."}>
+                    {values.message}
+                </Suspense>
+            </div>
+        </>
+    );
 }
