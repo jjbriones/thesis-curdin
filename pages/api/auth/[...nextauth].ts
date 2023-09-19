e2971665371db020b13a47ab7ccf82b6afd73b06
Jarl Briones <jarljbriones@gmail.com>
2023-09-20 00:32:11 +0800

    register functionality setup

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,65 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import { CredentialsProvider } from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+
+import prisma from '@/app/libs/prismadb';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password are required');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('No user found');
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isValid) {
+          throw new Error('Invalid password');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages:{
+    signIn: '/',
+  }
+  debug: process.env.NODE_ENV === 'development',
+  session:{
+    strategy: 'jwt',
+  },
+  secret:process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions)
