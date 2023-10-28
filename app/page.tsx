a9dc38d736daba38ab86886d6b0431bf343d7dc0
faustojc <61080389+faustojc@users.noreply.github.com>
2023-10-29 00:01:00 +0800

    Fetching prediction output

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,35 @@
 import { Suspense, useEffect, useState } from 'react';
 
 export default function Home() {
-  const [values, setValues] = useState({
-    message: '',
-  });
+  const [message, setMessage] = useState<number[]>([]);
+  const [score, setScore] = useState<number>(0);
 
   //fetch data from server
   useEffect(() => {
-    fetch('http://127.0.0.1:8080/api/home')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setValues({
-          message: data.message,
-        });
-      });
+      fetch('http://127.0.0.1:8080/api/predict')
+          .then((response) => response.json())
+          .then((data) => {
+                setMessage(data.prediction)
+          });
+
+        fetch('http://127.0.0.1:8080/api/score')
+            .then((response) => response.json())
+            .then((data) => {
+                setScore(data.score)
+            });
   }, []);
 
   return (
     <>
-      <div className={'font-bold'}>
-        Message from server:
-        <Suspense fallback={'Loading...'}>{values.message}</Suspense>
+      <div className={'p-4'}>
+        Prediction from Regression Model:
+        <Suspense fallback={'Loading...'}>
+            <p className={'mb-4'}>Score: {score * 100}</p>
+
+            {message.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))}
+        </Suspense>
       </div>
     </>
   );
