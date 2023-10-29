0cc4c035ccd54ae58366cbe7df9d5581c48a8d94
faustojc <61080389+faustojc@users.noreply.github.com>
2023-10-29 16:09:37 +0800

    Display predictions for each models

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,34 @@
 'use client';
-import { Suspense, useEffect, useState } from 'react';
+import {Suspense, useEffect, useState} from 'react';
 
 export default function Home() {
-  const [message, setMessage] = useState<number[]>([]);
-  const [score, setScore] = useState<number>(0);
+    const [message, setMessage] = useState({
+        predictions: [],
+        actualPrice: 0
+    });
 
-  //fetch data from server
-  useEffect(() => {
-      fetch('http://127.0.0.1:8080/api/predict')
-          .then((response) => response.json())
-          .then((data) => {
-                setMessage(data.prediction)
-          });
-
-        fetch('http://127.0.0.1:8080/api/score')
+    //fetch data from server
+    useEffect(() => {
+        fetch('http://127.0.0.1:8080/api/predict')
             .then((response) => response.json())
             .then((data) => {
-                setScore(data.score)
+                setMessage({
+                    predictions: data.predictions,
+                    actualPrice: data.actualPrice
+                })
             });
-  }, []);
+    }, []);
 
-  return (
-    <>
-      <div className={'p-4'}>
-        Prediction from Regression Model:
-        <Suspense fallback={'Loading...'}>
-            <p className={'mb-4'}>Score: {score * 100}</p>
-
-            {message.map((item, index) => (
-                <p key={index}>{item}</p>
-            ))}
-        </Suspense>
-      </div>
-    </>
-  );
+    return (
+        <div className={'p-4'}>
+            Prediction from Regression Model:
+            <Suspense fallback={'Loading...'}>
+                {Object.keys(message.predictions).map((value, index) => (
+                    <div key={index}>
+                        {index}: {value}
+                    </div>
+                ))}
+            </Suspense>
+        </div>
+    );
 }
