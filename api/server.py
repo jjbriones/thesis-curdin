ba9179f4f511edc67c8752915ff301be70367010
Jarl Briones <jarljbriones@gmail.com>
2023-10-31 12:20:34 +0800

    Update: Revise Carport and Yard

--- api/server.py
+++ api/server.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 CORS(app)
 
 # Load data
-df = pd.read_csv('data/1st_subd.csv')
-X = df[['AreaSQM', 'Floors', 'Bedrooms', 'Bathrooms', 'Yard', 'Carport']]
+df = pd.read_csv('./data/1st_subd.csv')
+X = df[['AreaSQM', 'Floors', 'Bedrooms', 'Bathrooms', 'Carport', 'Yard']]
 y = df[['Price_Php_M']]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
