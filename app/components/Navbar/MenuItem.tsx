04ac9657fbb5bbf15f565f32ae3d38e1bc12d075
faustojc <faustojcboko@gmail.com>
2023-11-14 17:00:07 +0800

    Added route to dashboard

--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -1,19 +1,21 @@
 'use client';
 
+import { ReactNode } from "react";
+
 interface MenuItemProps {
-  onClick: () => void;
-  content: string;
+    onClick: () => void;
+    content: string | ReactNode;
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ onClick, content }) => {
-  return (
-    <div
-      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
-      onClick={onClick}
-    >
-      {content}
-    </div>
-  );
+    return (
+        <div
+            className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+            onClick={onClick}
+        >
+            {content}
+        </div>
+    );
 };
 
 export default MenuItem;
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -2,7 +2,7 @@
 
 import { AiOutlineMenu } from 'react-icons/ai';
 import Image from 'next/image';
-import { useCallback, useState } from 'react';
+import { KeyboardEvent, useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
@@ -10,61 +10,67 @@ import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
 import Avatar from './Avatar';
 import useAddPropertyModal from '@/app/hooks/useAddPropertyModal';
+import Link from 'next/link';
 
 interface UserMenuProps {
-  currentUser?: SafeUser | null;
+    currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
-  const registerModal = useRegisterModal();
-  const loginModal = useLoginModal();
-  const addPropertyModal = useAddPropertyModal();
-  const [isOpen, setIsOpen] = useState(false);
+    const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+    const addPropertyModal = useAddPropertyModal();
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = useCallback(() => {
-    setIsOpen((prev) => !prev);
-  }, []);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((prev) => !prev);
+    }, []);
 
-  return (
-    <div className="relative">
-      <div className="flex flex-row items-center gap-3 ">
-        <div
-          onClick={toggleOpen}
-          className=" p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block">
-            <Avatar src={currentUser?.image} />
-          </div>
-        </div>
-      </div>
-      {isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[30vw] md:w-[20vw] bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">
-            {currentUser ? (
-              <>
-                <MenuItem
-                  onClick={addPropertyModal.open}
-                  content="Add Property"
-                />
-                <MenuItem onClick={() => {}} content="Favorites" />
-                <MenuItem onClick={() => {}} content="Reservations" />
-                <MenuItem onClick={() => {}} content="Interactive Dashboard" />
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3 ">
+                <div
+                    onClick={toggleOpen}
+                    onKeyDown={(event: KeyboardEvent<HTMLDivElement>) => {
+                        if (event.key === 'Enter') {
+                            toggleOpen();
+                        }
+                    }}
+                    className=" p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                >
+                    <AiOutlineMenu />
+                    <div className="hidden md:block">
+                        <Avatar src={currentUser?.image} />
+                    </div>
+                </div>
+            </div>
+            {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[30vw] md:w-[20vw] bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        {currentUser ? (
+                            <>
+                                <MenuItem
+                                    onClick={addPropertyModal.open}
+                                    content="Add Property"
+                                />
+                                <MenuItem onClick={() => { }} content="Favorites" />
+                                <MenuItem onClick={() => { }} content="Reservations" />
+                                <MenuItem onClick={() => { }} content={<Link href={'/dashboard'}>Dashboard</Link>} />
 
-                <hr />
-                <MenuItem onClick={() => signOut()} content="Logout" />
-              </>
-            ) : (
-              <>
-                <MenuItem onClick={loginModal.open} content="Login" />
-                <MenuItem onClick={registerModal.open} content="Sign Up" />
-              </>
+                                <hr />
+                                <MenuItem onClick={() => signOut()} content="Logout" />
+                            </>
+                        ) : (
+                            <>
+                                <MenuItem onClick={loginModal.open} content="Login" />
+                                <MenuItem onClick={registerModal.open} content="Sign Up" />
+                            </>
+                        )}
+                    </div>
+                </div>
             )}
-          </div>
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default UserMenu;
