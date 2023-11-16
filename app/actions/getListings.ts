dfcb17b701d01a480bae9febebfa5d964406bea4
faustojc <faustojcboko@gmail.com>
2023-11-16 17:07:59 +0800

    feat: Added listings estimated price

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,20 +2,39 @@ import prisma from '@/app/libs/prismadb';
 import { list } from 'postcss';
 
 export default async function getListings() {
-  try {
-    const listings = await prisma.listing.findMany({
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
+    try {
+        const listings = await prisma.listing.findMany({
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
 
-    const safeListings = listings.map((listing) => ({
-      ...listing,
-      createdAt: listing.createdAt.toISOString(),
-    }));
+        const features = listings.map((listing) => ({
+            AreaSQM: listing.area,
+            Floors: listing.floorCount,
+            Bedrooms: listing.roomCount,
+            Bathrooms: listing.bathroomCount,
+            Carport: listing.carport,
+            Yard: listing.yard
+        }));
 
-    return safeListings;
-  } catch (error: any) {
-    throw new Error(error);
-  }
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ model: 'Ridge', features: features }),
+        };
+
+        // const estimatedPrice = await fetch('http://127.0.0.1:8080/api/estimate', options);
+
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+        }));
+
+        return safeListings;
+    } catch (error: any) {
+        throw new Error(error);
+    }
 }
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -10,51 +10,51 @@ import { list } from 'postcss';
 import { useMemo } from 'react';
 
 interface ListingClientProps {
-  reservations?: Reservation[];
-  listing: SafeListing & {
-    user: SafeUser;
-  };
-  currentUser?: SafeUser | null;
+    reservations?: Reservation[];
+    listing: SafeListing & {
+        user: SafeUser;
+    };
+    currentUser?: SafeUser | null;
 }
 
 const ListingClient: React.FC<ListingClientProps> = ({
-  listing,
-  currentUser,
+    listing,
+    currentUser,
 }) => {
-  const category = useMemo(() => {
-    return categories.find((items) => items.label === listing.category);
-  }, [listing.category]);
+    const category = useMemo(() => {
+        return categories.find((items) => items.label === listing.category);
+    }, [listing.category]);
 
-  return (
-    <Container>
-      <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-6">
-          <ListingHead
-            title={listing.title}
-            imageSrc={listing.imageSrc}
-            locationValue={listing.locationValue}
-            id={listing.id}
-            currentUser={currentUser}
-            key={listing.id}
-          />
-          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
-            <ListingInfo
-              user={listing.user}
-              category={category}
-              description={listing.description}
-              roomCount={listing.roomCount}
-              bathroomCount={listing.bathroomCount}
-              locationValue={listing.locationValue}
-              price={listing.price}
-              area={listing.area}
-              carport={listing.carport}
-              yard={listing.yard}
-            />
-          </div>
-        </div>
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                        key={listing.id}
+                    />
+                    <div className="grid grid-cols-1 mt-6">
+                        <ListingInfo
+                            user={listing.user}
+                            category={category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                            price={listing.price}
+                            area={listing.area}
+                            carport={listing.carport}
+                            yard={listing.yard}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default ListingClient;
