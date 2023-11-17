1812015d0a5878ce833ae94873407f39d6c250f4
Jarl Briones <jarljbriones@gmail.com>
2023-11-18 00:10:32 +0800

    feat: added my properties and delete functionality

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,41 +1,52 @@
 import prisma from '@/app/libs/prismadb';
-import { list } from 'postcss';
 
-export default async function getListings() {
-    try {
-        const listings = await prisma.listing.findMany({
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
+export interface IListingsParams {
+  userId?: string;
+}
 
-        const features = listings.map((listing) => ({
-            AreaSQM: listing.area,
-            Floors: listing.floorCount,
-            Bedrooms: listing.roomCount,
-            Bathrooms: listing.bathroomCount,
-            Carport: listing.carport,
-            Yard: listing.yard
-        }));
+export default async function getListings(params: IListingsParams) {
+  try {
+    const { userId } = params;
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ model: 'Ridge', features: features }),
-        };
+    let query: any = {};
 
-        // TODO: to be fixed, DO NOT UNCOMMENT
-        // const estimatedPrice = await fetch('http://127.0.0.1:8080/api/estimate', options);
-
-        const safeListings = listings.map((listing) => ({
-            ...listing,
-            createdAt: listing.createdAt.toISOString(),
-        }));
-
-        return safeListings;
-    } catch (error: any) {
-        throw new Error(error);
+    if (userId) {
+      query.userId = userId;
     }
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    const features = listings.map((listing) => ({
+      AreaSQM: listing.area,
+      Floors: listing.floorCount,
+      Bedrooms: listing.roomCount,
+      Bathrooms: listing.bathroomCount,
+      Carport: listing.carport,
+      Yard: listing.yard,
+    }));
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ model: 'Ridge', features: features }),
+    };
+
+    // TODO: to be fixed, DO NOT UNCOMMENT
+    // const estimatedPrice = await fetch('http://127.0.0.1:8080/api/estimate', options);
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,42 @@
 import getCurrentUser from './actions/getCurrentUser';
-import getListings from './actions/getListings';
+import getListings, { IListingsParams } from './actions/getListings';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 import { SafeListing } from './types';
 
-export default async function Home() {
-    const listings = await getListings();
-    const currentUser = await getCurrentUser();
-
-    if (listings.length === 0) {
-        return (
-            <div>
-                <EmptyState showReset />
-            </div>
-        );
-    }
-
-    return (
-        <Container>
-            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                {listings.map((listing: SafeListing) => {
-                    return (
-                        <ListingCard
-                            currentUser={currentUser}
-                            key={listing.id}
-                            data={listing}
-                        />
-                    );
-                })}
-                <div>{listings.title}</div>
-            </div>
-        </Container>
-    );
+interface HomeProps {
+  searchParams: IListingsParams;
 }
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <div>
+        <EmptyState showReset />
+      </div>
+    );
+  }
+
+  return (
+    <Container>
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: SafeListing) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
+        <div>{listings.title}</div>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
