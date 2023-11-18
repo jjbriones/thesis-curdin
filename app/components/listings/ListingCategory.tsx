5c47dea2f9d96836511476c631cb76f715c2df40
Jarl Briones <jarljbriones@gmail.com>
2023-11-18 22:58:14 +0800

    fix: input correct details

--- app/components/listings/ListingCategory.tsx
+++ app/components/listings/ListingCategory.tsx
@@ -4,70 +4,61 @@ import { useEffect, useState } from 'react';
 import { IconType } from 'react-icons';
 
 interface ListingCategoryProps {
-    icon: IconType;
-    label: string;
-    description: string;
-    estimatedPrice?: number;
+  icon: IconType;
+  label: string;
+  description: string;
+  estimatedPrice?: number;
 }
 
 const ListingCategory: React.FC<ListingCategoryProps> = ({
-    icon: Icon,
-    label,
-    description,
-    estimatedPrice
+  icon: Icon,
+  label,
+  description,
+  estimatedPrice,
 }) => {
-    const [price, setPrice] = useState({
-        value: '',
-        discount: 0.0
-    });
+  const [price, setPrice] = useState({
+    value: '',
+    discount: 0.0,
+  });
 
-    useEffect(() => {
-        if (estimatedPrice === undefined) return;
+  useEffect(() => {
+    if (estimatedPrice === undefined) return;
 
-        if (label.includes('Luxury')) {
-            setPrice({
-                value: (estimatedPrice * 0.8).toFixed(2),
-                discount: 20
-            });
-        }
-        else if (label.includes('Standard')) {
-            setPrice({
-                value: (estimatedPrice * 0.75).toFixed(2),
-                discount: 35
-            });
-        }
-        else {
-            setPrice({
-                value: estimatedPrice.toFixed(2),
-                discount: 0
-            });
-        }
+    if (label.includes('Elegant')) {
+      setPrice({
+        value: (estimatedPrice * 0.8).toFixed(2),
+        discount: 20,
+      });
+    } else if (label.includes('Standard')) {
+      setPrice({
+        value: (estimatedPrice * 0.7).toFixed(2),
+        discount: 35,
+      });
+    } else {
+      setPrice({
+        value: estimatedPrice.toFixed(2),
+        discount: 0,
+      });
+    }
+  }, [label]);
 
-    }, [label]);
-
-    return (
-        <div className="flex justify-between">
-            <div className="flex flex-col gap-6">
-                <div className="flex flex-row items-center gap-4">
-                    <Icon size={40} className="text-neutral-600" />
-                    <div className="flex flex-col gap-1">
-                        <div className="text-xl font-semibold">{label}</div>
-                        <div className="text-neutral-500 font-light">{description}</div>
-                    </div>
-                </div>
-            </div>
-            <div className="flex items-center">
-                {price.discount > 0 && (
-                    <>
-                        <p className="text-neutral-500">P{price.value} million</p>
-                        <p className="flex items-center ml-1">
-                            (<span className='text-green-500'>-{price.discount}%</span>)
-                        </p>
-                    </>
-                )}
-            </div>
+  return (
+    <div className="flex justify-between">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-row items-center gap-4">
+          <Icon size={40} className="text-neutral-600" />
+          <div className="flex flex-col gap-1">
+            <div className="text-xl font-semibold">{label}</div>
+            <div className="text-neutral-500 font-light">{description}</div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex items-center">
+        <p className="text-neutral-500">P{price.value} million</p>
+        {price.discount > 0 && <p className="flex items-center ml-1"></p>}
+      </div>
+    </div>
+  );
 };
 
 export default ListingCategory;
--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -4,78 +4,106 @@ import { SafeUser } from '@/app/types';
 import { IconType } from 'react-icons';
 import ListingCategory from './ListingCategory';
 import dynamic from 'next/dynamic';
+import { useState, useEffect } from 'react';
 
 const Map = dynamic(() => import('../Map'), { ssr: false });
 interface ListingInfoProps {
-    user: SafeUser;
-    category:
+  user: SafeUser;
+  category:
     | {
         icon: IconType;
         label: string;
         description: string;
-    }
+      }
     | undefined;
 
-    description: string;
-    roomCount: number;
-    bathroomCount: number;
-    locationValue: string;
-    price: number;
-    area: number;
-    carport: number;
-    yard: boolean;
-    estimatedPrice?: number;
+  description: string;
+  roomCount: number;
+  bathroomCount: number;
+  locationValue: string;
+  price: number;
+  area: number;
+  carport: number;
+  yard: boolean;
+  estimatedPrice?: number;
 }
 
 const ListingInfo: React.FC<ListingInfoProps> = ({
-    user,
-    category,
-    description,
-    roomCount,
-    bathroomCount,
-    locationValue,
-    price,
-    area,
-    carport,
-    yard,
-    estimatedPrice,
+  user,
+  category,
+  description,
+  roomCount,
+  bathroomCount,
+  locationValue,
+  price,
+  area,
+  carport,
+  yard,
+  estimatedPrice,
 }) => {
-    return (
-        <div className="col-span-4 flex flex-col gap-8">
-            <div className="flex justify-between">
-                <div className="flex flex-col gap-2">
-                    <div className="text-xl font-semibold flex flex-row items-center gap-2">
-                        Property of {user?.name} | ₱ {price}
-                    </div>
-                    <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
-                        <div>{area} SQM</div>
-                        <div>{roomCount} Bedrooms</div>
-                        <div>{bathroomCount} Bathrooms</div>
-                        <div>{carport} Cars</div>
-                        <div>{yard ? 'Has' : 'No'} Yard</div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 text-right">
-                    <p className="font-semibold text-lg">Estimated Price</p>
-                    <p className="font-light text-neutral-500">₱ {estimatedPrice} million</p>
-                </div>
-            </div>
+  const [predictedPrice, setPredictedPrice] = useState({
+    value: estimatedPrice?.toFixed(2) ?? 0,
+    discount: 0.0,
+  });
 
-            <hr />
-            {category && (
-                <ListingCategory
-                    icon={category.icon}
-                    label={category.label}
-                    description={category.description}
-                    estimatedPrice={estimatedPrice}
-                />
-            )}
-            <hr />
-            <div className="text-lg font-light text-neutral-500">{description}</div>
-            <hr />
-            <Map center={[0, 0]} />
+  useEffect(() => {
+    if (estimatedPrice === undefined) return;
+
+    if (category?.label.includes('Elegant')) {
+      setPredictedPrice({
+        value: (estimatedPrice * 0.8).toFixed(2),
+        discount: 20,
+      });
+    } else if (category?.label.includes('Standard')) {
+      setPredictedPrice({
+        value: (estimatedPrice * 0.7).toFixed(2),
+        discount: 35,
+      });
+    } else {
+      setPredictedPrice({
+        value: estimatedPrice.toFixed(2),
+        discount: 0,
+      });
+    }
+  }, [category?.label]);
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-2">
+          <div className="text-xl font-semibold flex flex-row items-center gap-2">
+            Property of {user?.name} | ₱ {price}
+          </div>
+          <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+            <div>{area} SQM</div>
+            <div>{roomCount} Bedrooms</div>
+            <div>{bathroomCount} Bathrooms</div>
+            <div>{carport} Cars</div>
+            <div>{yard ? 'Has' : 'No'} Yard</div>
+          </div>
         </div>
-    );
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold text-lg">Estimated Price</p>
+          <p className="font-light text-neutral-500">
+            ₱ {predictedPrice.value} million
+          </p>
+        </div>
+      </div>
+
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+          estimatedPrice={estimatedPrice}
+        />
+      )}
+      <hr />
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+      <hr />
+      <Map center={[0, 0]} />
+    </div>
+  );
 };
 
 export default ListingInfo;
