161f812c1f5473c71098ce986dd8fdc09cbe83ea
Jarl Briones <jarljbriones@gmail.com>
2023-11-18 22:38:26 +0800

    fix: changed sign to Peso

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,46 +2,54 @@ import prisma from '@/app/libs/prismadb';
 import getEstimatedPrice from './getEstimatedPrice';
 import { Feature } from '../types';
 
-
 export interface IListingsParams {
-    userId?: string;
+  userId?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
-    try {
-        const { userId } = params;
+  try {
+    const { userId } = params;
 
-        let query: any = {};
+    let query: any = {};
 
-        if (userId) {
-            query.userId = userId;
-        }
-        const listings = await prisma.listing.findMany({
-            where: query,
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-
-        const features: Feature[] = listings.map((listing: {
-            area: number; floorCount: number; roomCount: number; bathroomCount: number; carport: number; yard: number;
-        }) => ({
-            'AreaSQM': listing.area,
-            'Floors': listing.floorCount,
-            'Bedrooms': listing.roomCount,
-            'Bathrooms': listing.bathroomCount,
-            'Yard': listing.yard,
-            'Carport': listing.carport
-        }));
-
-        const estimatedPrice: [] = await getEstimatedPrice('Lasso', features);
-
-        return listings.map((listing: { createdAt: { toISOString: () => any; }; }) => ({
-            ...listing,
-            createdAt: listing.createdAt.toISOString(),
-            estimatedPrice: estimatedPrice.shift(),
-        }));
-    } catch (error: any) {
-        throw new Error(error);
+    if (userId) {
+      query.userId = userId;
     }
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    const features: Feature[] = listings.map(
+      (listing: {
+        area: number;
+        floorCount: number;
+        roomCount: number;
+        bathroomCount: number;
+        carport: number;
+        yard: number;
+      }) => ({
+        AreaSQM: listing.area,
+        Floors: listing.floorCount,
+        Bedrooms: listing.roomCount,
+        Bathrooms: listing.bathroomCount,
+        Yard: listing.yard,
+        Carport: listing.carport,
+      })
+    );
+
+    const estimatedPrice: [] = await getEstimatedPrice('Lasso', features);
+
+    return listings.map(
+      (listing: { createdAt: { toISOString: () => any } }) => ({
+        ...listing,
+        createdAt: listing.createdAt.toISOString(),
+        estimatedPrice: estimatedPrice.shift(),
+      })
+    );
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
