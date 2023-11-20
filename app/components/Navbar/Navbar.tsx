783b8e3209ff39fdf079231ad92f394912327915
faustojc <61080389+faustojc@users.noreply.github.com>
2023-11-20 11:41:07 +0800

    Fix slow routing issue

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,38 +2,39 @@
 
 import Container from '../Container';
 import Image from 'next/image';
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import Search from './Search';
 import UserMenu from './UserMenu';
-import {SafeUser} from '@/app/types';
+import { SafeUser } from '@/app/types';
 import Categories from './Categories';
 import React from "react";
+import Link from 'next/link';
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
-    const router = useRouter();
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
     return (
         <nav className="w-full bg-white z-10 ">
             <div className="py-4 border-b-sm">
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                        <Image
-                            onClick={() => router.push('/')}
-                            src="/images/logo.png"
-                            alt="Curdin Logo"
-                            height="100"
-                            width="100"
-                            className="block cursor-pointer"
-                        />
-                        <Search/>
-                        <UserMenu currentUser={currentUser}/>
+                        <Link href="/">
+                            <Image
+                                src="/images/logo.png"
+                                alt="Curdin Logo"
+                                height="100"
+                                width="100"
+                                className="block cursor-pointer"
+                            />
+                        </Link>
+                        <Search />
+                        <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
-            <Categories/>
+            <Categories />
         </nav>
     );
 };
