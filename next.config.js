843b14b1cd1f36dc8489d4269e6a32215fc42260
faustojc <faustojcboko@gmail.com>
2024-02-12 23:32:57 +0800

    Add API rewrite domains

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'apps.googleusercontent.com',
-      'res.cloudinary.com',
-    ],
-  },
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: '/api/:path*',
+			},
+		];
+	},
+	images: {
+		domains: [
+			'lh3.googleusercontent.com',
+			'apps.googleusercontent.com',
+			'res.cloudinary.com',
+		],
+	},
 };
 
 module.exports = nextConfig;
