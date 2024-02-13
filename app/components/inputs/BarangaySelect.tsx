813345de5ba1f44959611f5a61fad2446a4a33ea
faustojc <faustojcboko@gmail.com>
2024-02-13 08:30:10 +0800

    Fix undefined type

--- app/components/inputs/BarangaySelect.tsx
+++ app/components/inputs/BarangaySelect.tsx
@@ -4,49 +4,49 @@ import useBarangay from '@/app/hooks/useBarangay';
 import Select from 'react-select';
 
 export type BarangaySelectValue = {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 };
 
 interface BarangaySelectProps {
-  value: BarangaySelectValue;
-  onChange: (value: BarangaySelectValue) => void;
+	value: BarangaySelectValue | undefined;
+	onChange: (value: BarangaySelectValue) => void;
 }
 
 const BarangaySelect: React.FC<BarangaySelectProps> = ({ value, onChange }) => {
-  const { getAll } = useBarangay();
+	const { getAll } = useBarangay();
 
-  return (
-    <div>
-      <h1>Barangay</h1>
-      <Select
-        placeholder="Barangay"
-        isClearable
-        options={getAll()}
-        value={value}
-        onChange={(value) => onChange(value as BarangaySelectValue)}
-        formatOptionLabel={(option: any) => (
-          <div className="flex flex-row items-center gap-3">
-            <div>{option.label}</div>
-          </div>
-        )}
-        classNames={{
-          control: () => 'p-3 border-2',
-          input: () => 'text-lg',
-          option: () => 'text-lg',
-        }}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 6,
-          colors: {
-            ...theme.colors,
-            primary: 'black',
-            primary25: '#ffe3e6',
-          },
-        })}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Barangay</h1>
+			<Select
+				placeholder="Barangay"
+				isClearable
+				options={getAll()}
+				value={value}
+				onChange={(value) => onChange(value as BarangaySelectValue)}
+				formatOptionLabel={(option: any) => (
+					<div className="flex flex-row items-center gap-3">
+						<div>{option.label}</div>
+					</div>
+				)}
+				classNames={{
+					control: () => 'p-3 border-2',
+					input: () => 'text-lg',
+					option: () => 'text-lg',
+				}}
+				theme={(theme) => ({
+					...theme,
+					borderRadius: 6,
+					colors: {
+						...theme.colors,
+						primary: 'black',
+						primary25: '#ffe3e6',
+					},
+				})}
+			/>
+		</div>
+	);
 };
 
 export default BarangaySelect;
